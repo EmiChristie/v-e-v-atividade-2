b7977931f8e2df5cd5ac897364111a5a8f32c55f
Emile Christie Dantas Pereira <emile.christie.pereira@ccc.ufcg.edu.br>
2024-07-30 08:17:20 -0300

    ciclo 1 do TDD

---
+++ src/processadorContas/Conta.java
@@ -0,0 +1,10 @@
+package processadorContas;
+
+import java.util.Date;
+
+public class Conta {
+
+    public Conta(int codigoConta, Date data, double valor){
+
+    }
+}
--- tests/processadorContas/ProcessadorContasTests.java
+++ tests/processadorContas/ProcessadorContasTests.java
@@ -3,20 +3,17 @@ package processadorContas;
 import org.junit.Test;
 import processadorContas.*;
 
+import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
 
 public class ProcessadorContasTests {
+    Conta conta1;
 
-    ProcessadorContas pc = new ProcessadorContas();
 
     @Test
-    public void processadorContasTeste1(){
-        double[] contas = new double[0];
-        double fatura = 0;
-        String processamento = pc.processar(contas,fatura);
-        String processamentoEsperado = "Fatura de 1.500,00 (20/02/2023) com 3 contas no valor de 500,00, " +
-                "400,00 e 600,00. As três contas foram pagas por boleto no dia 20/02/2023 (todas em dia), assim a fatura é marcada como PAGA.";
-
-        assertEquals(processamentoEsperado, processamento);
+    public void setup(){
+        Date dataConta1 = new Date(124,02,05);
+        conta1 = new Conta(1, dataConta1,500.00);
     }
 }
